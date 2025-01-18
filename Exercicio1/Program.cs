64966cab3291ef48fe66974f14d35a5ecb6fda77
GustavoLealDev <GustavoLealP@outlook.com>
2025-01-18 18:00:22 -0300

    Exercicio While

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+    class course {
+      class Program {
+        static void Main(string[] args) {
+
+            
+            Console.WriteLine("Digite o número para saber se é Positivo ou Negativo: ");
+            int numero = int.Parse(Console.ReadLine());
+            
+            while (numero != 2002) {
+                Console.WriteLine("Senha incorreta! Tente Novamente: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("ACESSO PERMITIDO!!");
+        }
+    }
+}

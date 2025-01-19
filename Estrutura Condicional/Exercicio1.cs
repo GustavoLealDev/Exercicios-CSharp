db0194347149d27096fd7b94bcf31b49b1e44b27
GustavoLealDev <GustavoLealP@outlook.com>
2025-01-18 21:17:11 -0300

    Exercicio if-else

--- Estrutura Condicional/Exercicio1.cs
+++ Estrutura Condicional/Exercicio1.cs
@@ -6,6 +6,7 @@ using System.Globalization;
         static void Main(string[] args) {
 
             
+
             Console.WriteLine("Digite o número para saber se é Positivo ou Negativo: ");
             int numero = int.Parse(Console.ReadLine());
             

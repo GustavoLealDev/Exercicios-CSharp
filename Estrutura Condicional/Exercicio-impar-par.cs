7f350f029d6f51d0f1d760450e7bd63791dd6153
GustavoLealDev <GustavoLealP@outlook.com>
2025-01-19 16:01:40 -0300

    Mais exercicios

---
+++ Estrutura Condicional/Exercicio-impar-par.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+    class course {
+      class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite o número para saber se é impar ou par: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) {
+                Console.WriteLine($"{x} é par");
+            } else {
+                Console.WriteLine($"{x} é impar");
+            }
+        }
+    }
+}

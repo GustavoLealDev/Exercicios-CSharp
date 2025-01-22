047c1c28bae5a40799b2426a77787fde85f6699b
GustavoLealDev <GustavoLealP@outlook.com>
2025-01-22 10:18:09 -0300

    Add Classe Pessoa e seu Construtor

---
+++ Classe, Objetos e Atributos/Exercicio1-Pessoa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Course
+{
+    internal class Pessoas
+    {
+        public string Name { get; set; }   
+        public int Idade { get; set; }
+
+    }
+}

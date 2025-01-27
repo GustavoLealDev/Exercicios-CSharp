06febca38b35375d950308d34a161daef417d288
GustavoLealDev <GustavoLealP@outlook.com>
2025-01-27 01:45:10 -0300

    Att

---
+++ Arrays e Listas/Livros.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Course
+{
+     class Livros
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoDePublicacao { get; set; }
+        public double Preco {  get; set; }
+
+        public Livros(string titulo, string autor, int anoDePublicacao, double preco)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            AnoDePublicacao = anoDePublicacao;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return "Livro : " + Titulo + 
+                ", Autor: " + Autor + 
+                ", Ano: " + AnoDePublicacao + 
+                ", Preço: " + Preco;
+        }
+    }
+}

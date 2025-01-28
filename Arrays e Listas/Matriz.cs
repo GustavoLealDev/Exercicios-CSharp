4cbda226e060c5ee023c2ffb9dc306c214cf2dd9
GustavoLealDev <GustavoLealP@outlook.com>
2025-01-28 14:23:07 -0300

    Att

--- Arrays e Listas/Matriz.cs
+++ Arrays e Listas/Matriz.cs
@@ -7,7 +7,9 @@ namespace Course
         static void Main(string[] args) {
 
             Console.WriteLine("Digite valor de Linhas e Colunas: ");
+            Console.Write("Digite o número de linhas: ");
             int M = int.Parse(Console.ReadLine());
+            Console.Write("Digite o número de colunas: ");
             int N = int.Parse(Console.ReadLine());
 
             int[,] mat = new int [M, N];
@@ -21,12 +23,39 @@ namespace Course
                     mat[i, j] = int.Parse(values[j]);
                 }
             }
+            
+            
+            Console.WriteLine();
+            Console.WriteLine("Digite um número para saber a posição:");
+            int x = int.Parse(Console.ReadLine());
 
-            foreach (int i in mat) 
-            { 
-                Console.WriteLine(i);
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Posição: " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Número a esquerda: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Número acima: " + mat[i - 1, j]);
+                        }
+                        if (j < N - 1)
+                        {
+                            Console.WriteLine("Número a direita: " + mat[i, j + 1]);
+                        }
+                        if (i < M - 1)
+                        {
+                            Console.WriteLine("Número abaixo: " + mat[i + 1, j]);
+                        }
+                        Console.WriteLine();
+                    }
+                }
             }
-
         }
     }
 }

03590fa4af483db7b0c08ecb3e315787b6f81af9
GustavoLealDev <GustavoLealP@outlook.com>
2025-01-29 23:48:32 -0300

    Att

---
+++ POO/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}

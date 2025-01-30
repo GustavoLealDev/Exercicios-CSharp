6f811f27dfcb099e82f69701a27ad8d9f5364fa8
GustavoLealDev <GustavoLealP@outlook.com>
2025-01-29 23:58:51 -0300

    Att

---
+++ POO/Entities/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities.Enums;
+
+namespace Course.Entities
+{
+     class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public Department Department { get; set; }
+        public double BaseSalary { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker(string name, WorkerLevel level, Department department, double baseSalary)
+        {
+            Name = name;
+            Level = level;
+            Department = department;
+            BaseSalary = baseSalary;
+        }
+
+        public void AddContract (HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void Removeontract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+    }
+}

5999c6f998c019e204a0f503d253d05a5aa73baf
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-04 14:35:05 -0300

    Att

---
+++ POO Products/Produtos/Entities/ImportedProduct.cs
@@ -0,0 +1,26 @@
+﻿namespace Course.Entities
+{
+     class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+        
+        public ImportedProduct() 
+        { 
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return CustomsFee + Price; 
+        }
+        public override string priceTag()
+        {
+            return Name + " $ " + TotalPrice() + " (Custom fee: $ " + CustomsFee + ")";
+        }
+
+    }
+}

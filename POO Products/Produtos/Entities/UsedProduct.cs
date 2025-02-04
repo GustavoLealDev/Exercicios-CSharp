c59674c404d68cf42cab1c6479bb309fa6e34b39
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-04 14:31:09 -0300

    Att

---
+++ POO Products/Produtos/Entities/UsedProduct.cs
@@ -0,0 +1,21 @@
+﻿namespace Course.Entities
+{
+     class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+    
+        public UsedProduct() 
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string priceTag()
+        {
+            return Name + " (Used) $ " + Price.ToString("F2") + ", " + "(Manufacture date: " + ManufactureDate.ToString("dd/MM/yyyy") + ")"; 
+        }
+    }
+}

6c271544bf909405a44b3cef44d74ec4fcde5a9a
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-07 15:55:10 -0300

    Att

---
+++ POO/POO Tax/Imposto/Entities/IndividualTax.cs
@@ -0,0 +1,27 @@
+﻿namespace Course.Entities
+{
+    class IndividualTax : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public IndividualTax(double healthExpenditures, string name, double anualIncome) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+        public override double Tax()
+        {
+            if (AnualIncome < 20000)
+            {
+                return AnualIncome * 0.15;
+            }
+            else if (AnualIncome > 20000 && HealthExpenditures > 0)
+            {
+                return (AnualIncome * 0.25) - (HealthExpenditures * 0.50);
+            }
+            else 
+            {
+               return AnualIncome * 0.25;
+            }
+        }
+    }
+}

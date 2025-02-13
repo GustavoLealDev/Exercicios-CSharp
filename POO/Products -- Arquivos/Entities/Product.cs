396766e37bcca2701677ca99a920e11dfcc4e755
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-12 23:54:39 -0300

    Att

---
+++ POO/Products -- Arquivos/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿ namespace Course.Entities
+    {
+        class Product
+        {
+
+            public string Nome { get; set; }
+            public double Preco { get; set; }
+            public int Quantidade { get; set; }
+
+            public Product(string nome, double preco, int quantidade)
+            {
+                Nome = nome;
+                Preco = preco;
+                Quantidade = quantidade;
+            }
+
+            public double Total()
+            {
+                return Preco * Quantidade;
+            }
+        }
+    }
+
+

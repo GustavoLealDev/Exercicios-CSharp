000816291f3b78015cb3bf8c468e81b6f29fd900
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-15 04:19:54 -0300

    Att

---
+++ Interfaces/Parcela/Services/PaypalService.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Pipes;
+
+namespace Course.Services
+{
+    class PaypalService : ITaxService
+    {
+        public double FeePeercentage = 0.02;
+        public double FeeInterest = 0.01;
+
+        public double PaymentFee(double amount)
+        {
+            return amount * FeePeercentage;
+        }
+
+        public double Interest(double amount, int mounths)
+        {
+            return amount * FeeInterest * mounths;
+        }
+    }
+}

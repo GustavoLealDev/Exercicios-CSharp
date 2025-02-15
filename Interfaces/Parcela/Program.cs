1610b7ffb4d63d75a115f3dddfb14da7cefe4ff5
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-15 04:30:28 -0300

    Att

---
+++ Interfaces/Parcela/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Course.Entities;
+using Course.Services;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER CONTRACT DATA: ");
+            Console.Write("ID: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract Value: ");
+            double contractValue = double.Parse(Console.ReadLine());
+            Console.Write("Enter number of installments: ");
+            int mounths = int.Parse(Console.ReadLine());
+
+            Contract contract = new Contract(id, date, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(contract, mounths);
+
+            Console.WriteLine("INSTALLMENTS: ");
+            foreach (Installment installment in contract.Installments) 
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}

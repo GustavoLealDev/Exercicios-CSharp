66c40cee73ef0c4ee7bfeae045dac14e40dd1b19
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-15 04:22:54 -0300

    Att

---
+++ Interfaces/Parcela/Services/ContractService.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Course.Entities;
+
+namespace Course.Services
+{
+    class ContractService
+    {
+        private ITaxService _taxService;
+        public ContractService(ITaxService taxService)
+        {
+            _taxService = taxService;
+        }
+
+        public void ProcessContract(Contract contract, int mounths)
+        {
+            double sum = contract.ContractValue / mounths;
+            for (int i = 1; i <= mounths; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updatSum = sum + _taxService.Interest(sum, i);
+                double fullSum = updatSum + _taxService.PaymentFee(updatSum);
+                contract.AddInstallments(new Installment(date, fullSum));
+            }
+        }
+
+    }
+}

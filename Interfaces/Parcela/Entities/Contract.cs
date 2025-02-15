8f0b34f7b186a43c9f238c2a42e1e021852256b6
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-15 04:13:53 -0300

    Att

---
+++ Interfaces/Parcela/Entities/Contract.cs
@@ -0,0 +1,22 @@
+﻿namespace Course.Entities
+{
+    class Contract
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double ContractValue { get; set; }
+        public List<Installment> Installments { get; set; } = new List<Installment>();
+
+        public Contract(int id, DateTime date, double contractValue)
+        {
+            Id = id;
+            Date = date;
+            ContractValue = contractValue;
+            Installments = new List<Installment>();
+        }
+        public void AddInstallments(Installment installment)
+        {
+            Installments.Add(installment);
+        }
+    }
+}

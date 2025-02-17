b0c6884143673430230d5dccaaa4a46ecc17a910
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-16 23:01:31 -0300

    Att

---
+++ Interfaces/Vehicle/Entities/Stock.cs
@@ -0,0 +1,50 @@
+﻿using Course.Services;
+
+namespace Course.Entities
+{
+    class Stock
+    {
+        private List<IVehicle> vehicles;
+
+        public Stock()
+        {
+            vehicles = new List<IVehicle>();
+        }
+
+        public void AddVehicle(IVehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+            Console.WriteLine("vehicle added successfully");
+        }
+
+        public void RemoveVehicle(string model)
+        {
+            IVehicle vehicle = vehicles.FirstOrDefault(x => x != null);
+            if (vehicles != null)
+            {
+                vehicles.Remove(vehicle);
+                Console.WriteLine("vehicle removed successfully");
+            }
+            else
+            {
+                Console.WriteLine("Vehicle not found!");
+            }
+        }
+
+        public void ListVehicle()
+        {
+            if (vehicles.Count > 0)
+            {
+                Console.WriteLine("List of Vehicle: ");
+                foreach (Vehicle vehicle in vehicles)
+                {
+                    vehicle.Details();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Empty stock.");
+            }
+        }
+    }
+}

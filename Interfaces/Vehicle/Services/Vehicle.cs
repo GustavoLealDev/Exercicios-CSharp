6df6347ca3ff55257860270d2eda4fab6b6b3e93
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-16 22:52:11 -0300

    Att

---
+++ Interfaces/Vehicle/Services/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using Course.Services;
+
+namespace Course.Entities
+{
+    abstract class Vehicle : IVehicle
+    {
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double Fip { get; set; }
+        public Vehicle(string mark, string model, int year, double fip)
+        {
+            Mark = mark;
+            Model = model;
+            Year = year;
+            Fip = fip;
+        }
+
+        public abstract void Details();
+    }
+}

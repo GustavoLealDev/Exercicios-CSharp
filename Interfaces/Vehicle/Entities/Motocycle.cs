1478d01db80d7b5d48c350428a0b4ad2ce72ecf6
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-16 22:59:35 -0300

    Att

---
+++ Interfaces/Vehicle/Entities/Motocycle.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+    class Motocycle : Vehicle
+    {
+        public Motocycle(string mark, string model, int year, double fip) : base(mark, model, year, fip) { }
+        public override void Details()
+        {
+            Console.WriteLine($"Motocycle: {Mark} | Model: {Model} | Year: {Year} | Value: {Fip.ToString("F3")}");
+        }
+    }
+}

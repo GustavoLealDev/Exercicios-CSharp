57802b09acc226ea5c2090238ffeef15423cb91d
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-16 22:57:59 -0300

    Att

---
+++ Interfaces/Vehicle/Entities/Trucker.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+    class Trucker : Vehicle
+    {
+        public Trucker(string mark, string model, int year, double fip) : base(mark, model, year, fip) { }
+        public override void Details()
+        {
+            Console.WriteLine($"Car: {Mark} | Model: {Model} | Year: {Year} | Value: {Fip.ToString("F3")}");
+        }
+    }
+}

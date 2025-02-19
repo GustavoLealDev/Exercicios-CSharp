8ede8a8b92df490457debc1f64b01686fae4f2bd
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-19 01:40:44 -0300

    Att

---
+++ Arquivos/Employee/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Projetos\in.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                List<Employee> list = new List<Employee>();
+                while (!sr.EndOfStream)
+                {
+                    list.Add(new Employee(sr.ReadLine()));
+                }
+                list.Sort();
+                foreach (Employee emp in list)
+                {
+                    Console.WriteLine(emp);
+                }
+            }
+        }
+    }
+}

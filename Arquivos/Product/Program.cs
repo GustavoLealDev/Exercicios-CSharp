62e312ac0fa37fadb21c32ef4f8359f0c81c63b3
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-19 01:48:35 -0300

    Att

---
+++ Arquivos/Product/Program.cs
@@ -0,0 +1,49 @@
+﻿using Course.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter file: ");
+            string sourceFile = Console.ReadLine();
+
+            try
+            {
+                string[] file = File.ReadAllLines(sourceFile);
+
+                string sourceFolder = Path.GetDirectoryName(sourceFile);
+                string targetFolder = sourceFolder + @"\out";
+                string targetFile = targetFolder + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolder);
+
+                using (StreamWriter sw = File.AppendText(targetFile))
+                {
+                    foreach (string line in file)
+                    {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1]);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine(product.Name + "," + product.Total().ToString("F2"));
+                    }
+                }
+            }
+            catch (IOException error)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}

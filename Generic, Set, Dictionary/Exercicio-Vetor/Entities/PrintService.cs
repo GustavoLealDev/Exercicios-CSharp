1c75efc2d9840e8d074c72c34c7e7e39f49eae6f
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-20 23:28:19 -0300

    Att

---
+++ Generic, Set, Dictionary/Exercicio-Vetor/Entities/PrintService.cs
@@ -0,0 +1,32 @@
+﻿namespace Course.Entities
+{
+    class PrintService
+    {
+        public int[] _value = new int[10];
+        public int _count = 0;
+
+        public void AddValue(int value)
+        {
+            _value[_count] = value;
+            _count++;
+        }
+        public int First()
+        {
+            return _value[0];
+        }
+        public void Print()
+        {
+            Console.WriteLine("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.WriteLine(_value[i] + ",");
+            }
+            if (_count > 0)
+            {
+                Console.WriteLine(_value[_count - 1]);
+            }
+            Console.WriteLine("]");
+            Console.WriteLine();
+        }
+    }
+}

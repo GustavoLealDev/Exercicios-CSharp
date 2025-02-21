42909cb59f0eecb665677039ec03d2751ce47492
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-21 02:33:58 -0300

    Att

---
+++ Generic, Set, Dictionary/Product/Services/CalculationService.cs
@@ -0,0 +1,18 @@
+﻿namespace Course.Entities
+{
+    class CalculationService
+    {
+        public T Max<T>(List<T> list) where T : IComparable
+        {
+            T max = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}

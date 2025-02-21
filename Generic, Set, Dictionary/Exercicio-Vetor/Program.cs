169c9cb5e313a212cb58da5d854eee8c7842ffec
GustavoLealDev <GustavoLealP@outlook.com>
2025-02-20 23:34:41 -0300

    Att

---
+++ Generic, Set, Dictionary/Exercicio-Vetor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write($"Value {i}: ");
+                int x = int.Parse(Console.ReadLine());  
+                printService.AddValue(x);
+            }
+
+            Console.WriteLine("First Value: " + printService.First());
+            Console.WriteLine("Max Value: " + printService.Max());
+        }
+    }
+}

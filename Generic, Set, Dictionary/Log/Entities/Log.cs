cd1f582a48aee48c972461ba84daf7bb5dd46889
GustavoLealDev <GustavoLealP@outlook.com>
2025-03-02 05:50:39 -0300

    Att

---
+++ Generic, Set, Dictionary/Log/Entities/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Course.Entities
+{
+    class Log
+    {
+
+        public string Name { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Log))
+            {
+                return false;
+            }
+            Log other = obj as Log;
+            return Name.Equals(other.Name);
+        }
+    }
+}

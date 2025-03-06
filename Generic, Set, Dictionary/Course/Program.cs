6fd29a9d756bcebc0878321f9ff9059d2420e7ae
GustavoLealDev <GustavoLealP@outlook.com>
2025-03-05 21:25:41 -0300

    Att

---
+++ Generic, Set, Dictionary/Course/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter file path: ");
+            string path = Console.ReadLine();
+
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+
+                Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                while (!streamReader.EndOfStream)
+                {
+
+                    string[] Record = streamReader.ReadLine().Split(',');
+                    string candidate = Record[0];
+                    int votes = int.Parse(Record[1]);
+
+                    if (dictionary.ContainsKey(candidate))
+                    {
+                        dictionary[candidate] += votes;
+                    }
+                    else
+                    {
+                        dictionary[candidate] = votes;
+                    }
+                }
+
+                foreach (var item in dictionary)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+        }
+    }
+}

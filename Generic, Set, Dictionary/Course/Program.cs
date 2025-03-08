ffdf2f9af64c8c85e33650e445a18f345a99269a
GustavoLealDev <GustavoLealP@outlook.com>
2025-03-08 01:13:09 -0300

    Att

--- Generic, Set, Dictionary/Course/Program.cs
+++ Generic, Set, Dictionary/Course/Program.cs
@@ -7,43 +7,55 @@ namespace Course
     {
         static void Main(string[] args)
         {
-
             HashSet<int> direito = new HashSet<int>();
             HashSet<int> administração = new HashSet<int>();
             HashSet<int> turismo = new HashSet<int>();
 
-            Console.Write("Quantos alunos fazem Direito? ");
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
+            try
             {
-                Console.Write($"Código do aluno {i}: ");
-                int codigo = int.Parse(Console.ReadLine());
-                direito.Add(codigo);
+                Console.Write("Quantos alunos fazem Direito? ");
+                int n = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write($"Código do aluno {i}: ");
+                    int codigo = int.Parse(Console.ReadLine());
+                    direito.Add(codigo);
+                }
+
+                Console.WriteLine();
+                Console.Write("Quantos alunos fazem Administração? ");
+                n = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write($"Código do aluno {i}: ");
+                    int codigo = int.Parse(Console.ReadLine());
+                    administração.Add(codigo);
+                }
+
+                Console.WriteLine();
+                Console.Write("Quantos alunos fazem Turismo? ");
+                n = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write($"Código do aluno {i}: ");
+                    int codigo = int.Parse(Console.ReadLine());
+                    turismo.Add(codigo);
+                }
+
+                Console.WriteLine();
+                HashSet<int> all = new HashSet<int>(direito);
+                all.UnionWith(administração);
+                all.UnionWith(turismo);
+                Console.WriteLine("Total de Estudantes: " + all.Count);
             }
-            Console.WriteLine();
-            Console.Write("Quantos alunos fazem Administração? ");
-            n = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
+            catch (FormatException)
             {
-                Console.Write($"Código do aluno {i}: ");
-                int codigo = int.Parse(Console.ReadLine());
-                administração.Add(codigo);
+                Console.WriteLine("Erro: Entrada inválida. Por favor, insira um número válido.");
             }
-            Console.WriteLine();
-            Console.Write("Quantos alunos fazem Turismo? ");
-            n = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
+            catch (Exception ex)
             {
-                Console.Write($"Código do aluno {i}: ");
-                int codigo = int.Parse(Console.ReadLine());
-                turismo.Add(codigo);
+                Console.WriteLine("Ocorreu um erro inesperado: " + ex.Message);
             }
-            
-            Console.WriteLine();
-            HashSet<int> all = new HashSet<int>(direito);
-            all.UnionWith(administração);
-            all.UnionWith(turismo);
-            Console.WriteLine("Total de Estudantes: " + all.Count);
         }
     }
 }
